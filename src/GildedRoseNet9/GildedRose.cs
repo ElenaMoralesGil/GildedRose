241a118bb3a8d8f43db775320db9b99fc48e50a5
MiguelEscSan <miguelescobedosantana2@gmail.com>
2025-02-19 10:51:36 +0000

    refactor: extracted is sulfurance condition to global

--- src/GildedRoseNet9/GildedRose.cs
+++ src/GildedRoseNet9/GildedRose.cs
@@ -39,55 +39,12 @@ namespace GildedRose.Console
         {
             foreach (Item item in Items)
             {   
-
-
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
+                if(item.Name != "Sulfuras, Hand of Ragnaros") {
+                    if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        item.Quality = item.Quality - 1; 
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Quality > 0)
                         {
-                            if (item.SellIn < 11 )
-                            {
-                                item.Quality = item.Quality + 1;                      
-                            }
-
-                            if (item.SellIn < 6 )
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != "Aged Brie")
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                    item.Quality = item.Quality - 1;
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
+                            item.Quality = item.Quality - 1; 
                         }
                     }
                     else
@@ -95,10 +52,50 @@ namespace GildedRose.Console
                         if (item.Quality < 50)
                         {
                             item.Quality = item.Quality + 1;
+
+                            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                            {
+                                if (item.SellIn < 11 )
+                                {
+                                    item.Quality = item.Quality + 1;                      
+                                }
+
+                                if (item.SellIn < 6 )
+                                {
+                                    item.Quality = item.Quality + 1;
+                                }
+                            }
+                        }
+                    }
+
+                    item.SellIn = item.SellIn - 1;
+
+                    if (item.SellIn < 0)
+                    {
+                        if (item.Name != "Aged Brie")
+                        {
+                            if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                            {
+                                if (item.Quality > 0)
+                                {
+                                        item.Quality = item.Quality - 1;
+                                }
+                            }
+                            else
+                            {
+                                item.Quality = item.Quality - item.Quality;
+                            }
+                        }
+                        else
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
                         }
                     }
                 }
-            }
+            }     
         }
 
     //       public void UpdateQuality()

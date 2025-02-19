04dbf690ce8cdee999f07bee331bc5de1c472eb9
MiguelEscSan <miguelescobedosantana2@gmail.com>
2025-02-19 10:18:46 +0000

    create: create golden master use cases

--- GildedRose.Tests/UnitTest1.cs
+++ GildedRose.Tests/UnitTest1.cs
@@ -22,22 +22,33 @@ public class Tests
                           {
                               Items = new List<Item>
                                           {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6},
-                                              new Item { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 2, Quality =10},
-                                              new Item{Name = "Cool elixir", SellIn = -2, Quality = 3 },
-                                              new Item{Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -2, Quality = 3 },
-                                              new Item{Name = "Aged Brie", SellIn = -2, Quality = 3 }
-                                        
+                                            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                                            new Item{Name = "Aged Brie", SellIn = -2, Quality = 3 },
+                                            new Item{Name = "Aged Brie", SellIn = -1, Quality = 51},
+                                            new Item{Name = "Aged Brie", SellIn = 4, Quality = -1},
+
+                                            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                                            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 50},
+
+                                            // new Item{Name = "Conjured Mana Cake", SellIn = 2, Quality = 0},
+                                            // new Item{Name = "Conjured Mana Cake", SellIn = 3, Quality = 6},
+                                            // new Item{Name = "Conjured Mana Cake", SellIn = -4, Quality = 0},
+                                            // new Item{Name = "Conjured Mana Cake", SellIn = -4, Quality = 10},
+                                            // new Item{Name = "Conjured Mana Cake", SellIn = -4, Quality = 60},
+                                            
+                                            new Item{Name = "Cool elixir", SellIn = -2, Quality = 3 },
+                                            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                                            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                                            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = -1},  
+                                            new Item {Name = "Elixir of the Mongoose", SellIn = -2, Quality = 5}, 
+                                            new Item {Name = "Elixir of the Mongoose", SellIn = 0, Quality = 5},
+
+                                            new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
+                                            new Item { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 2, Quality = 10 },
+                                            new Item{Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -2, Quality = 3 },
+                                            new Item{Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -2, Quality = 60 },
+                                            new Item{Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -2, Quality = -1 },
+                                            new Item{Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 7, Quality = 30 },
                                           }
 
                           };

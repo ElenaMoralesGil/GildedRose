162a2c52727a5a52e6be64ab09d79f95743e91b4
MiguelEscSan <miguelescobedosantana2@gmail.com>
2025-02-19 12:50:02 +0000

    delete: delete unnecessary items in Main

--- src/GildedRoseNet9/GildedRose.cs
+++ src/GildedRoseNet9/GildedRose.cs
@@ -8,78 +8,36 @@ namespace GildedRose.Console
         public static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
-
-            var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-
-                          };
-
-            app.UpdateQuality();
-
-            System.Console.ReadKey();
-
         }
 
         public int UpdateBackStageQuality(Item item){
 
-            if(item.SellIn <= 0){
-                return 0;
-            }
+            if(item.SellIn <= 0) return 0;
 
             if(item.Quality  <50 ){
                 item.Quality++;
-                if (item.SellIn < 11 )
-                {
-                    item.Quality = item.Quality + 1;                      
-                }
+                if (item.SellIn < 11 ) item.Quality++;
 
-                if (item.SellIn < 6 )
-                {
-                    item.Quality = item.Quality + 1;
-                }
+                if (item.SellIn < 6 )item.Quality++;
             }
 
-    
-            
             return item.Quality;
         }
 
         public int UpdateNormalQuality(Item item){
-            if (item.Quality > 0 )
-            {
-                item.Quality = item.Quality - 1;
-            }
-            
-            if (item.SellIn <= 0){
-                item.Quality = item.Quality - 1;
-            }
+            if (item.Quality > 0 ) item.Quality--;
+        
+            if (item.SellIn <= 0) item.Quality--;
     
             return item.Quality;
         }
 
         public int UpdateAgedBrieQuality(Item item){
             if(item.Quality >=50) return item.Quality;
-            if (item.Quality < 50)
-            {
-                item.Quality = item.Quality + 1;
-            }
-            if (item.SellIn <= 0){
-                item.Quality = item.Quality + 1;
-            }
+
+            if (item.Quality < 50) item.Quality++;
+
+            if (item.SellIn <= 0) item.Quality++;
 
             return item.Quality;
         }
